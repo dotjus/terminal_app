a03a83aadf69cf52570bbc5dc31ff7eda63de05e
“Justin <dotjus@gmail.com>
2020-10-03 11:20:48 +1000

    added single_room_method_test.rb, and updated rooms.rb so that rooms are no longer classes. Instead they are each a collection of JSON files displayed by a single method.

--- rooms.rb
+++ rooms.rb
@@ -3,173 +3,158 @@ require 'fileutils'
 
 $prompt = TTY::Prompt.new
 
-
 def continue_story    
          
     puts " "                                                                                                      
     print "press any key to continue..."                                                                                                    
     STDIN.getch                                                                                                              
     print "            \r" # extra space to overwrite in case next sentence is short    
-    system "clear"        
+    system "clear" 
+    display_room     
 
 end 
 
 
+# def collect_item(current_room, item_no)
 
-class Room
-    # attr_accessor :display_room
+#     if $examine_collectable[examine_num] == true
+#         menu = $prompt.select("Do you want to take it?", %w(yes no))
+#         if menu == "yes"
+#             # $player_inventory << $examine_names[x]
 
-    def initialize(name)
-        @name = name
+#             ## Read file in
+#             file = File.read("player_data/" + $current_player_profile + "/examine_options/" + current_room + ".json")
+#             parsed = JSON.parse(file)
+#             ## Add picked to players inventory
+#             $player_inventory << parsed[item_no]["name"]
+#             ## Delete picked 
+#             parsed.delete_at(examine_num)
+#             ## Write elements - picked back into file
+#             File.open("player_data/" + $current_player_profile + "/examine_options/" + current_room + ".json","w") do |f|
+#                 f.write(parsed.to_json)
+#                 end
 
+#             p $examine_names[examine_num]
+#             puts "Your current inventory is #{$player_inventory}."
+#             return 5
+#         else
+#         end
+#     else
+#     end
+
+# end
+
+def display_room
+    ## Clear screen
+    system "clear" 
+
+    ## Clear room data
+    $room_numbers = []
+    $door_names = {}
+    $room_names = []
+
+    $examine_numbers = []
+    $examine_names = []
+    $examine_descriptions = []
+    $examine_collectable = []
+
+    ###### Print Title and Description
+    File.foreach("player_data/" + $current_player_profile + "/descriptions/" + $current_room + ".txt") { |line| puts line } 
+    # @examine_options
+
+    ###### Examine Options
+    file = File.read("player_data/" + $current_player_profile + "/examine_options/" + $current_room + ".json")
+    examine_array = JSON.parse(file)
+    
+    examine_array.each_with_index do |hash, index|  # could print just the keys(door names) then have the values to change current room 
+        
+        $player_inventory.each do |item|
+            
+            $examine_numbers << index
+            $examine_names << hash["name"]
+            $examine_descriptions << hash["examine"]
+            $examine_collectable << hash["collectable"]
+
+        end
     end
-
-    def collect_item(x)
-
-        if $examine_collectable[x] == true
-            menu = $prompt.select("Do you want to take it?", %w(yes no))
-            if menu == "yes"
-                # $player_inventory << $examine_names[x]
     
-                ## Read file in
-                file = File.read("player_data/" + $current_player_profile + "/examine_options/" + @name + ".json")
-                parsed = JSON.parse(file)
-                ## Add picked to players inventory
-                $player_inventory << parsed[x]["name"]
-                ## Delete picked 
-                parsed.delete_at(x)
-                ## Write elements - picked back into file
-                File.open("player_data/" + $current_player_profile + "/examine_options/" + @name + ".json","w") do |f|
-                    f.write(parsed.to_json)
-                    end
+    ###### Movement Options
+    file = File.read("player_data/" + $current_player_profile + "/move_options/" + $current_room + ".json")
+    move_array = JSON.parse(file)
+    move_array.each_with_index do |hash, index|  # could print just the keys(door names) then have the values to change current room 
+        $room_numbers << index
+        $door_names[hash["description"].to_sym] = index
+        $room_names << hash["destination"]
+    end
     
-                p $examine_names[x]
-                puts "Your current inventory is #{$player_inventory}."
-                return 5
-            else
-            end
+
+    ###### User Options
+    room_options = {examine: 1, move: 2}
+    menu = $prompt.select("What would you like to do?", room_options)
+    
+    if menu == 1
+        
+        testy = $prompt.select("Examine Options;", $examine_names)
+        
+        case testy
+
+        when $examine_names[0]          ## will need to add -1 or make 0 return to previous menu
+            puts $examine_descriptions[0]
+            # collect_item(0)
+            continue_story 
+
+        when $examine_names[1]
+            puts $examine_descriptions[1]
+            # collect_item(1)
+            continue_story 
+            
+        when $examine_names[2]
+            puts $examine_descriptions[2]
+            # collect_item(2)
+            continue_story 
+
+        when $examine_names[3]
+            puts $examine_descriptions[3]
+            # collect_item(3)
+            $x = 3
+            p $x
+            return $x
+            
         else
+            puts "nothing to see here"
+            continue_story 
+
         end
+            
+    elsif menu == 2
+        $menu = $prompt.select("Move Options;", $door_names)  
+
+        case $menu 
+
+        when $room_numbers[0]          ## will need to add -1 or make 0 return to previous menu
+            $room_name = $room_names[0]
+            return $room_name
+
+        when $room_numbers[1]
+            $room_name = $room_names[1]
+            return $room_name
+            
+        when $room_numbers[2]
+            $room_name = $room_names[2]
+            return $room_names[2]
+
+        else
+            puts "invalid input"
+
+        end
+
+    # elsif menu == 3
+    #     puts "Fight!"
     
     end
-
-    def display_room
-        ## Clear screen
-        system "clear" 
-
-        ## Clear room data
-        $room_numbers = []
-        $door_names = {}
-        $room_names = []
-
-        $examine_numbers = []
-        $examine_names = []
-        $examine_descriptions = []
-        $examine_collectable = []
-
-        ###### Print Title and Description
-        File.foreach("player_data/" + $current_player_profile + "/descriptions/" + @name + ".txt") { |line| puts line } 
-        # @examine_options
-
-        # ###### Examine Options
-        file = File.read("player_data/" + $current_player_profile + "/examine_options/" + @name + ".json")
-        examine_array = JSON.parse(file)
-        
-        examine_array.each_with_index do |hash, index|  # could print just the keys(door names) then have the values to change current room 
-            
-            $player_inventory.each do |item|
-                
-                $examine_numbers << index
-                $examine_names << hash["name"]
-                $examine_descriptions << hash["examine"]
-                $examine_collectable << hash["collectable"]
-                
-
-                
-            end
-        end
-        
-        ###### Movement Options
-        file = File.read("player_data/" + $current_player_profile + "/move_options/" + @name + ".json")
-        move_array = JSON.parse(file)
-        move_array.each_with_index do |hash, index|  # could print just the keys(door names) then have the values to change current room 
-            $room_numbers << index
-            $door_names[hash["description"].to_sym] = index
-            $room_names << hash["destination"]
-        end
-
-    end
-
-    def user_action
-        
-        room_options = {examine: 1, move: 2}
-        menu = $prompt.select("What would you like to do?", room_options)
-        
-        if menu == 1
-            
-            testy = $prompt.select("Examine Options;", $examine_names)
-            
-            case testy
-
-            when $examine_names[0]          ## will need to add -1 or make 0 return to previous menu
-                puts $examine_descriptions[0]
-                # collect_item(0)
-                continue_story 
-
-            when $examine_names[1]
-                puts $examine_descriptions[1]
-                collect_item(1)
-                continue_story 
-                
-            when $examine_names[2]
-                puts $examine_descriptions[2]
-                collect_item(2)
-                continue_story 
-
-            when $examine_names[3]
-                puts $examine_descriptions[3]
-                # collect_item(3)
-                $x = 3
-                p $x
-                return $x
-                
-            else
-                puts "nothing to see here"
-                continue_story 
-
-            end
-                
-        elsif menu == 2
-            $menu = $prompt.select("Move Options;", $door_names)
-
-            case $menu 
-
-            when $room_numbers[0]          ## will need to add -1 or make 0 return to previous menu
-                $room_name = $room_names[0]
-                return $room_name
-
-            when $room_numbers[1]
-                $room_name = $room_names[1]
-                return $room_name
-                
-            when $room_numbers[2]
-                $room_name = $room_names[2]
-                return $room_names[2]
-
-            else
-                puts "invalid input"
-
-            end
-
-        # elsif menu == 3
-        #     puts "Fight!"
-
-               
-        end
-
-    end
-
 
 end
+
+
 
 
